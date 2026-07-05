95818d092c7a4956944812e6af8bda5edbdfbfe0
Amy Peralta <AmyJimenaPeraltaAranda@gmail.com>
2026-07-05 02:17:53 -0500

    Corrige prefijo de variables de entorno para Vite

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Usar las variables de entorno
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
