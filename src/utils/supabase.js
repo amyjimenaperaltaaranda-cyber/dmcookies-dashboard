c9b19b8770f60f4d7b1a1afeb93e451ca470a173
Amy Peralta <AmyJimenaPeraltaAranda@gmail.com>
2026-07-05 01:43:57 -0500

    Agrega componentes base de Dashboard, Consultas, About y Objectives2

---
+++ src/utils/supabase.js
@@ -0,0 +1,8 @@
+// src/utils/supabase.js
+import { createClient } from '@supabase/supabase-js';
+
+// Usar las variables de entorno
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
